b45c338aeb93924d52214d4d2f39177b51157628
Dhurba Bhusal <dbhusal@emory.edu>
2025-01-31 17:18:59 -0600

    pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./contexts/AuthContext.js";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload now?")) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline");
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -17,6 +18,33 @@ export default defineConfig({
       },
     },
     react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "Restaurant Subscription App",
+        short_name: "MealPlans",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Enable PWA for development
+      },
+    }),
   ],
 
   optimizeDeps: {
